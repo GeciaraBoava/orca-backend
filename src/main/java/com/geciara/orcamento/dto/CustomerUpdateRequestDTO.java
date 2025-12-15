3c0e6c081ca59195365f2eb2926d9d2ae8d6b2e6
geciara <geciara.boava@rpisolucoes.com.br>
2025-12-15 16:01:40 -0300

    🐛  :: [Request] :: Corrige modal de updateRequest para supplier e customer

--- src/main/java/com/geciara/orcamento/dto/CustomerUpdateRequestDTO.java
+++ src/main/java/com/geciara/orcamento/dto/CustomerUpdateRequestDTO.java
@@ -1,15 +1,11 @@
 package com.geciara.orcamento.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerUpdateRequestDTO {
-
-    @NotNull
-    private Long id;
 
     private String name;
     private String cnpjCpf;
